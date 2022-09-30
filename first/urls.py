f3241cfb0be37f72164f7ad6c288a7883a26b272
Your Name <you@example.com>
2022-09-30 20:00:21 +0600

    add names

--- first/urls.py
+++ first/urls.py
@@ -14,6 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     Tom Holland
     Henry Danger
+
+    Blum
+    Ben ten
+    Dora
+
 """
 from django.contrib import admin
 from django.urls import path
